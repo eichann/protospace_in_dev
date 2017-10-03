8b64faf5522cfd8db35c35bac72dcb953df6b903
eichann <k.abe.in401@gmail.com>
2017-10-03 18:24:24 +0900

    ストロングパラメータなどを記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:name, :profile, :position, :occupation, :avatar)
+    devise_parameter_sanitizer.for(:account_update).push(:name, :profile, :position, :occupation, :avatar)
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
