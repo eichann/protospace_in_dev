0c38b12ded1db5a95d737c4bb1beaebe4536cb07
eichann <k.abe.in401@gmail.com>
2017-10-03 18:33:55 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'prototypes/index'
-
-  get 'prototypes/new'
-
-  get 'prototypes/sho'
-
   root 'prototypes#index'
 
   get  '/prototypes/index'  =>    'prototypes#index'
   get  '/prototypes/new'    =>    'prototypes#new'
   get  '/prototypes/show'   =>    'prototypes#show'
+
+  resources :users, only: [:show]
 end
