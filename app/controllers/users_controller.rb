513d63af326997901e7e5ea6a880a3234392b52d
eichann <k.abe.in401@gmail.com>
2017-10-04 14:15:32 +0900

    ルーティング、コントローラーを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: show
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    edirect_to :root
   end
 
   private
@@ -9,4 +18,17 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :position,
+      :profile,
+      :occupation,
+      :avatar
+    )
+  end
+
+
 end
